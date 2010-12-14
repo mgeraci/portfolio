8766994772218a0bcd07e0a02bbfbc9143b459c7
Michael P. Geraci <michael@reducify.com>
2010-12-13 19:30:18 -0500

    changed connect code for dreamhost sql

--- includes/connect.php
+++ includes/connect.php
@@ -1,24 +1,24 @@
 <?php
-	// // // Connect to database // // //
+  // // // Connect to database // // //
 
-	if (preg_match('/michaelgeraci\.com/', $_SERVER['SERVER_NAME'] )){
-		// online variabls
-		$username = "mgeraci";
-		$password = 'XdPfnS2N3@_N4rOl';
-		$host = "michaelg.ipowermysql.com";
-		$database = "mg";
-	} else {
-		// local variables
-		$username = "root";
-		$password = "";
-		$host = "";
-		$database = "michaelgeraci.com_development";
-	}
+  if (preg_match('/michaelgeraci\.com/', $_SERVER['SERVER_NAME'])){
+    // online variabls
+    $username = "mgeraci";
+    $password = "3dN7hNejLLUeoqVLermf";
+    $host = "mysql.michaelgeraci.dreamhosters.com";
+    $database = "michaelgeraci_production";
+  } else if (preg_match('/localhost/', $_SERVER['SERVER_NAME'])) {
+    // local variables
+    $username = "root";
+    $password = "";
+    $host = "";
+    $database = "michaelgeraci.com_development";
+  }
 
-	$conn = mysql_connect($host, $username, $password);
-	mysql_select_db($database, $conn);
+  $conn = mysql_connect($host, $username, $password);
+  mysql_select_db($database, $conn);
 
-	if (!$conn) {
-		echo "Could Not Connect to the Graphic Design Database";
-	}	
+  if (!$conn) {
+    echo "Could Not Connect to the Graphic Design Database";
+  }
 ?>
