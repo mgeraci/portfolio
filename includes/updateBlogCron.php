79b1710c5329aacdcb06e70d7ba4cb4b690e9d20
Michael P. Geraci <michael@reducify.com>
2010-12-19 17:01:50 -0500

    added update blog cron file, removed phpinfo

---
+++ includes/updateBlogCron.php
@@ -0,0 +1,29 @@
+#!/usr/local/bin/php -q
+
+<?php
+  // connect to db
+  include('./connect.php');
+
+  // get the number of rows with visible == zero
+  $query = "SELECT count(id) as count FROM blog WHERE visible=0";
+  $result = mysql_query($query);
+
+  if (!$result) {
+    echo 'Could not run query: ' . mysql_error();
+    exit;
+  }
+
+  while ($row = mysql_fetch_assoc($result)) {
+    $count = $row['count'];
+  }
+
+  // if there are some with invisible, publish the lowest numbered one
+  if ($count > 0) {
+    $query = "UPDATE blog SET visible=1 WHERE visible=0 ORDER BY number ASC LIMIT 1";
+    $result = mysql_query($query);
+    if (!$result) {
+      echo 'Could not run query: ' . mysql_error();
+      exit;
+    }
+  }
+?>
