2819385a981e8fc6c51e83216b53275c8a7dc011
Michael P. Geraci <mgeraci@gmail.com>
2011-12-21 19:41:38 -0500

    added array structure

--- extras/topAlbums2011/index.php
+++ extras/topAlbums2011/index.php
@@ -21,48 +21,43 @@
       <div class="wrapIn">
         <h2>I know you don't care, but here are</h2>
         <h1>My Top Records of 2011</h1>
+        <?php
+          $albums = array(
+            // artist, title, image, rambling
+            array('James Blake', 'James Blake', "james_blake.jpg", 'This record is crushingly beautiful. Sometimes when I listen to it I can\'t do anything else, I just have to sit there. This record makes me want to be an alcoholic.'),
+            array('Wye Oak', 'Civilian', "wye_oak.jpg", 'poop'),
+            array('Peter Bjorn and John', 'Gimme Some', "pbj.jpg", 'snicjerksdf')
+          );
+        ?>
         <ul id='albums'>
-          <li>
-            <span class='count'>1</span>
-            <img src=''>
-            <div class='content'></div>
-          </li>
-          <li>
-            <span class='count'>2</span>
-            <img src=''>
-            <div class='content'></div>
-          </li>
-          <li>
-            <span class='count'>3</span>
-            <img src=''>
-            <div class='content'></div>
-          </li>
-          <li>
-            <span class='count'>4</span>
-            <img src=''>
-            <div class='content'></div>
-          </li>
-          <li>
-            <span class='count'>5</span>
-            <img src=''>
-            <div class='content'></div>
-          </li>
-          <li>
-            <span class='count'>6</span>
-            <img src=''>
-            <div class='content'></div>
-          </li>
-          <li>
-            <span class='count'>7</span>
-            <img src=''>
-            <div class='content'></div>
-          </li>
+          <?php
+            $i = 1;
+            
+            foreach($albums as $album) {
+              $artist = $album[0];
+              $title = $album[1];
+              $image = $album[2];
+              $text = $album[3];
+              
+              echo "
+                <li>
+                  <span class='count'>$i</span>
+                  <img src='./images/$image'>
+                  <div class='content'>
+                    <h1>$title</h1>
+                    <h2>by <span>$artist</span></h2>
+                    <div class='text'>$text</div>
+                  </div>
+                </li>
+              ";
+              
+              $i += 1;
+            }
+          ?>
         </ul>
         <div id='thanks'>
           that's it. thanks for reading.
           <br>complain via <a href='mailto:mgeraci@gmail.com'>email</a> or <a href='http://twitter.com/mgeraci' target='_blank'>twitter</a>.
-          <br>
-          <br>made with some love, but mostly spite and snobbery, by michael p. geraci
         </div>
       </div>
     </div>
