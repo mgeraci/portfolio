54bda2b22d5f93878d815616c94b293e11e30784
Michael P. Geraci <mgeraci@gmail.com>
2012-05-14 14:22:08 -0400

    add audio links to winterpills, update google analytics code

--- extras/winterpills/index.php
+++ extras/winterpills/index.php
@@ -8,6 +8,32 @@
 
   include("../../includes/head.php");
 ?>
+  <style type='text/css'>
+    #audio a, #audio img, #audio span{
+      float: left;
+    }
+
+    #audio img{
+      width: 100px;
+    }
+
+    #audio a{
+      text-decoration: none;
+      margin-left: 90px;
+    }
+
+    #audio a:hover, #audio a:active{
+      text-decoration: none !important;
+    }
+
+    #audio span{
+      text-align: left;
+      font-size: 25px;
+      position: relative;
+      top: 25px;
+    }
+
+  </style>
   <body>
     <div id="menuWrapper" class="wrapOut">
       <div class="wrapIn">
@@ -20,6 +46,28 @@
     <div id="contentWrapper" class="wrapOut">
       <div class="wrapIn" style='text-align:center;width:960px;'>
         <h1>Winterpills in Westchester</h1>
+        <br>
+        <br>
+        <div id='audio'>
+          <a href='./winterpillz_flac.zip' onClick='_gaq.push(["_trackEvent", "Winterpills Audio", "flac"])'>
+            <img src='./zip.png'>
+            <span>
+              Audio from the concert
+              <br>flac - 456mb
+            </span>
+          </a>
+          <a href='./winterpillz_mp3.zip' onClick='_gaq.push(["_trackEvent", "Winterpills Audio", "mp3"])'>
+            <img src='./zip.png'>
+            <span>
+              Audio from the concert
+              <br>192kbps mp3 - 122mb
+            </span>
+          </a>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
         <br>
         <br>
         <img src='/extras/winterpills/winterpills_geraci_1.jpg'>
