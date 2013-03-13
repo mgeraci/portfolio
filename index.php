467b97e8fbc7f91a3c2b4b56568fe19d08dd423b
Michael P. Geraci <me@mgeraci.com>
2013-03-13 13:20:16 -0400

    update recent

--- index.php
+++ index.php
@@ -54,10 +54,10 @@
     </div>
     <div id="indexBottom" class="wrapOut">
       <div class="wrapIn">
-        <h1>Last Updated: 2.13.13</h1>
+        <h1>Last Updated: 3.13.13</h1>
         <?php
           $recent = array(
-            array('/photography/blog/392', '/media/photography/blog/thumbs/6538.jpg', "New Photos:<br>California Hikes and Brooklyn Bridge Park", ''),
+            array('/photography/blog/414', '/media/photography/blog/thumbs/6987.jpg', "New Photos:<br>Pictured Rocks National Lakeshore & Chicago", ''),
             array('/web/dtime2', '/images/recent_thumbs/dtime2.jpg', "New Web Design:<br>Dtime Version 2", ''),
             array('/graphic/51', '/media/graphic/thumbs/pumptrain.jpg', "New Graphic Design:<br>Pump Train", ''),
             array('http://mgeraci.github.com/Coffee-Filter/', '/images/recent_thumbs/coffee-filter.png', "New Open Source JavaScript Library:<br>Coffee Filter", 'new-window'),
