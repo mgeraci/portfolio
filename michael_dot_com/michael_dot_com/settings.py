bada5ec0dced6d095f8a05c3d079820d216194f0
Michael P. Geraci <me@mgeraci.com>
2016-02-13 15:42:32 -0500

    add a test stylesheet

--- michael_dot_com/michael_dot_com/settings.py
+++ michael_dot_com/michael_dot_com/settings.py
@@ -70,6 +70,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+
                 'portfolio.context_processors.menu',
             ],
         },
