4de318d355cde7302786f65a9ab549106a812b27
Michael P. Geraci <me@mgeraci.com>
2016-02-20 21:02:25 -0500

    add recent updates section to index

--- michael_dot_com/portfolio/views.py
+++ michael_dot_com/portfolio/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render
+from michael_dot_com.localsettings import STATIC_URL
 
 # main pages
 def index(request):
-    return render(request, 'pages/index.html')
+    recent_image_root = '{}images/recent_thumbs/'.format(STATIC_URL)
+
+    recent_items = [
+        {
+            'text': ['My favorite albums of 2015'],
+            'image': '{}albums-2015.jpg'.format(recent_image_root),
+            'href': 'http://albums.michaelgeraci.com/list/2015',
+        },
+        {
+            'text': ['New project:', 'Smart mirror display'],
+            'image': '{}auto-hud.jpg'.format(recent_image_root),
+            'href': 'https://github.com/mgeraci/auto-hud',
+        },
+        {
+            'text': ['New steel drum performance:', 'CASYM - Big People Party'],
+            'image': '{}casym-2014.jpg'.format(recent_image_root),
+            'href': 'https://www.youtube.com/watch?v=UaLBILfJ5ag',
+        },
+        {
+            'text': ['New Photos:', 'Summer trip to Duluth'],
+            'image': '{}7352.jpg'.format(recent_image_root),
+            'href': 'http://michaelgeraci.com/photography/blog/427',
+        },
+    ]
+
+    context = {
+        'recent_items': recent_items,
+        'recent_date': '2.19.16',
+    }
+
+    return render(request, 'pages/index.html', context)
 
 def web(request):
     return render(request, 'pages/web.html')
