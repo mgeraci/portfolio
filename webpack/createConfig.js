ddafb86b1322375ea3804c4d59f02f3c883347cb
Michael P. Geraci <me@mgeraci.com>
2016-10-29 10:36:36 -0400

    move webpack config to a creator function in advance of minification

---
+++ webpack/createConfig.js
@@ -0,0 +1,81 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const WebpackNotifierPlugin = require("webpack-notifier");
+
+function createConfig(params) {
+	const isProduction = params.isProduction;
+	const min = isProduction ? ".min" : "";
+
+  const entries = {
+		photoblog: "./michael_dot_com/portfolio/static/js/photoblog/Photoblog.jsx",
+		app: "./michael_dot_com/portfolio/static/js/app.coffee",
+	};
+
+	const output = {
+    path: "./michael_dot_com/portfolio/static/js/build/",
+    filename: "[name]" + min + ".js"
+  };
+
+  const resolve = {
+    extensions: ["", ".webpack.js", ".web.js", ".js", ".jsx", ".scss", ".sass"]
+  };
+
+	const eslint = {
+		emitError: true,
+		emitWarning: true,
+	};
+
+  modules = {
+		preLoaders: [
+			{
+				test: /\.jsx?$/,
+				loader: "eslint-loader",
+				exclude: [
+					/node_modules/,
+					/coffee/,
+					/throttle\.js/,
+					/vendor/,
+				],
+			}
+		],
+
+    loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: [/node_modules/],
+				loader: "babel-loader",
+				query: {
+					presets: ['es2015', 'react', 'stage-2']
+				}
+			}, {
+				test: /\.coffee$/,
+				loader: "coffee-loader",
+			}, {
+				test: /\.sass$/,
+				exclude: [/node_modules/],
+				loader: ExtractTextPlugin.extract("css!sass")
+			}, {
+				test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
+				loader: "file?name=../../../static/fonts/[name].[ext]"
+			}
+		]
+  };
+
+	plugins = [
+		new ExtractTextPlugin('../../css/build/styles.css', {
+			allChunks: true
+		}),
+    new WebpackNotifierPlugin(),
+  ];
+
+	return {
+		debug: !isProduction,
+		entry: entries,
+		output: output,
+		resolve: resolve,
+		eslint: eslint,
+		module: modules,
+		plugins: plugins,
+	};
+}
+
+module.exports = createConfig;
