478f1b544d97e9dcc42c1b7884363e5d2563cddd
Michael P. Geraci <me@mgeraci.com>
2016-11-15 23:11:14 -0500

    styles on the album list page, reduce text size on mobile, url change

--- portfolio/albums/urls.py
+++ portfolio/albums/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^albums/?$', views.album_lists, name='album_lists_url'),
-    url(r'^albums/list/(?P<slug>[^/]+)/$', views.album_list, name='album_list_url'),
+    url(r'^albums/(?P<slug>[^/]+)/$', views.album_list, name='album_list_url'),
 ]
