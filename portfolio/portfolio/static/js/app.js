fa2190564fcc683d152198992cd862c4412c79a4
Michael P. Geraci <me@mgeraci.com>
2020-05-24 20:48:01 -0500

    write modernizr out of the app

--- portfolio/portfolio/static/js/app.js
+++ portfolio/portfolio/static/js/app.js
@@ -1,12 +1,18 @@
 /* global document */
 
 import FastClick from "fastclick";
-import modernizr from "./vendor/modernizr"; // eslint-disable-line no-unused-vars
 
 import menu from "./menu";
 import lazyImages from "./lazy_images";
 import audio from "./audio";
 import video from "./video";
+
+// add a touchevents support class to the root for some minor style changes
+if ("ontouchstart" in document.documentElement) {
+	document.documentElement.classList.add("touchevents");
+} else {
+	document.documentElement.classList.add("no-touchevents");
+}
 
 menu.init();
 lazyImages.search();
